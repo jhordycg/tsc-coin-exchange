b5a113bf89be22f26f495a4fe8faedef42f1f951
Jhordy Caceres <jhordycg@pm.me>
2022-03-04 15:20:20 -0500

    basic + security + jwt

---
+++ src/test/java/com/challenge/tsc/coin_exchange/TscCoinExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.tsc.coin_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TscCoinExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
